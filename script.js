d87a108e8f6359a9d01f329b635d93b8bb1fb96d
Dmytro <coulheyman@gmail.com>
2025-08-26 22:30:12 +0300

    Style section-8 and add js code for its fq-section

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ document.getElementById("prev").addEventListener("click", () => {
   index = (index - 1 + totalSlides) % totalSlides;
   images.style.transform = `translateX(-${index * 100}%)`;
 });
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const btn = item.querySelector("button");
+
+  item.addEventListener("click", () => {
+    const isActive = item.classList.contains("active");
+
+    faqItems.forEach(i => {
+      i.classList.remove("active");
+      i.querySelector("button").textContent = "+";
+    });
+
+    if (!isActive) {
+      item.classList.add("active");
+      btn.textContent = "−";
+    }
+  });
+});
